d07456ee6a9933f930b510bdc1111cb5dd277778
mmladenova <maya.v.mladenova@gmail.com>
2025-10-21 18:02:28 +0200

    basic-typescript-operations

---
+++ basic-types.ts
@@ -0,0 +1,14 @@
+// let vs const
+let name = "Maya";
+const age = 25;
+name = "John";
+console.log(name);
+console.log(age);
+
+
+let numOne: number = 5;
+let numTwo: number = -5;
+// Ternary operator 
+let isPositiveNum: string = numOne > 0 ? "The number is Positive" : "The number is negative";
+console.log(isPositiveNum);
+console.log(numTwo);
